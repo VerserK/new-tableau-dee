30775f7b6f2a0e6184baded5e5fbe76218dbfacc
akarawatp <akarawat.p@siamkubota.co.th>
2023-10-09 17:16:30 +0700

    PrDocumentItem run

--- PrDocumentItem/__init__.py
+++ PrDocumentItem/__init__.py
@@ -12,4 +12,4 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
-    # PrDocumentItem.run()
+    PrDocumentItem.run()
